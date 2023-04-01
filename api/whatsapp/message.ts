527d6637b6cc0b700137a1d02e0004caf2742f43
Leonel Baltazar Bravo <96897286+pybalt@users.noreply.github.com>
2023-03-31 21:59:30 -0600

    Feat/add simple conversation   (#1)
    * adding the first sample conversation.
    
    * closing bracket to connect try and catch
    
    * fix(message): remove unneeded logic, remove broken if statements and format code.
    
    ---------
    
    Co-authored-by: NicolasMontone <montonenicolas01@gmail.com>

--- api/whatsapp/message.ts
+++ api/whatsapp/message.ts
@@ -1,11 +1,20 @@
 import type { VercelApiHandler, VercelResponse } from '@vercel/node'
 
-import { Whatsapp, sendMessageToPhoneNumber } from '../../lib/whatsapp'
+import {
+  Whatsapp,
+  sendMessageToPhoneNumber,
+  sendSimpleButtonsMessage,
+} from '../../lib/whatsapp'
 
 import {
   WhatsappNewMessageEventNotificationRequest,
   WhatsappParsedMessage,
 } from './types'
+
+const hasAssociatedWallet = (_recipientPhone: string) => {
+  // do something
+  return true // just for testing
+}
 
 const handler: VercelApiHandler = async (
   req: WhatsappNewMessageEventNotificationRequest,
@@ -32,22 +41,58 @@ const handler: VercelApiHandler = async (
       } = data
 
       if (typeOfMessage === 'text_message') {
-        const message = `Hey ${recipientName}, thanks for reaching out! We will get back to you shortly.`
+        const message = `¡Hola! ${recipientName}, soy tu crypto-bot favorito. Tu servicio
+        de billetera digital más seguro, confiable y fácil de usar.`
+
         await sendMessageToPhoneNumber(recipientPhone, message)
+        if (hasAssociatedWallet(recipientPhone)) {
+          await sendSimpleButtonsMessage(recipientPhone, '¿Qué deseas hacer?', [
+            { title: 'Recibir dinero', id: 'receive_money' },
+            { title: 'Enviar dinero', id: 'send_money' },
+            { title: 'Consultar saldo', id: 'check_balance' },
+          ])
+        } else {
+          await sendSimpleButtonsMessage(
+            recipientPhone,
+            'Veo que no tienes una billetera asociada a este número. ¿Deseas crear una?',
+            [{ title: 'Crear una billetera', id: 'create_wallet' }],
+          )
+        }
       }
 
       if (typeOfMessage === 'simple_button_message') {
-        // button
+        const button_id = data.message.button_reply.id
+        switch (button_id) {
+          case 'receive_money':
+            await sendMessageToPhoneNumber(
+              recipientPhone,
+              'Recibiendo dinero...',
+            )
+            break
+          case 'send_money':
+            // do something
+            await sendMessageToPhoneNumber(
+              recipientPhone,
+              `Hemos enviado tu dinero. Tu saldo es ...`,
+            )
+            break
+          case 'check_balance':
+            // do something
+            await sendMessageToPhoneNumber(recipientPhone, `Tu balance es ...`)
+            break
+          default:
+            break
+        }
       }
 
       // note: important to mark message as read to avoid duplicate messages
       await Whatsapp.markMessageAsRead({
         message_id: messageId,
       })
-    }
 
-    res.status(200).send('ok')
-    return
+      res.status(200).send('ok')
+      return
+    }
   } catch (error) {
     console.error({ error })
     res.status(500)
