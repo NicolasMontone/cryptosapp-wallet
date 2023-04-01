6e5042b1ea8525eede01b5e8f0d2d0420b8abf64
NicolasMontone <montonenicolas01@gmail.com>
2023-04-01 00:58:54 -0300

    fix(message): remove unneeded logic, remove broken if statements and format code.

--- api/whatsapp/message.ts
+++ api/whatsapp/message.ts
@@ -1,16 +1,19 @@
 import type { VercelApiHandler, VercelResponse } from '@vercel/node'
 
-import { Whatsapp, sendMessageToPhoneNumber, sendSimpleButtonsMessage } from '../../lib/whatsapp'
+import {
+  Whatsapp,
+  sendMessageToPhoneNumber,
+  sendSimpleButtonsMessage,
+} from '../../lib/whatsapp'
 
 import {
   WhatsappNewMessageEventNotificationRequest,
   WhatsappParsedMessage,
 } from './types'
-import { IncomingMessage } from 'http'
 
-const hasAssociatedwallet = (recipientPhone: string) => {
+const hasAssociatedWallet = (_recipientPhone: string) => {
   // do something
-  return true; // just for testing
+  return true // just for testing
 }
 
 const handler: VercelApiHandler = async (
@@ -39,50 +42,46 @@ const handler: VercelApiHandler = async (
 
       if (typeOfMessage === 'text_message') {
         const message = `¡Hola! ${recipientName}, soy tu crypto-bot favorito. Tu servicio
-        de billetera digital más seguro, confiable y fácil de usar.
-        ¿Qué deseas hacer?`
+        de billetera digital más seguro, confiable y fácil de usar.`
 
-        if(hasAssociatedwallet(recipientPhone)) {
-          await sendSimpleButtonsMessage(recipientPhone, message, [
-            {title: 'Recibir dinero', id: 'receive_money'},
-            {title: 'Enviar dinero', id: 'send_money'},
-            {title: 'Consultar saldo', id: 'check_balance'},
-            {title: 'Consultar transacciones', id: 'check_transactions'}
+        await sendMessageToPhoneNumber(recipientPhone, message)
+        if (hasAssociatedWallet(recipientPhone)) {
+          await sendSimpleButtonsMessage(recipientPhone, '¿Qué deseas hacer?', [
+            { title: 'Recibir dinero', id: 'receive_money' },
+            { title: 'Enviar dinero', id: 'send_money' },
+            { title: 'Consultar saldo', id: 'check_balance' },
           ])
         } else {
-        const message = `¡Hola! ${recipientName}, soy tu crypto-bot favorito. Tu servicio
-        de billetera digital más seguro, confiable y fácil de usar.
-        Veo que no tienes una billetera asociada a este número. ¿Deseas crear una?`
-        await sendSimpleButtonsMessage(recipientPhone, message, [	
-          {title: 'Crear una billetera', id: 'create_wallet'}
-          ]
+          await sendSimpleButtonsMessage(
+            recipientPhone,
+            'Veo que no tienes una billetera asociada a este número. ¿Deseas crear una?',
+            [{ title: 'Crear una billetera', id: 'create_wallet' }],
           )
+        }
       }
+
       if (typeOfMessage === 'simple_button_message') {
-        const button_id = data.message.button_reply.id;
+        const button_id = data.message.button_reply.id
         switch (button_id) {
           case 'receive_money':
-            // We generate a QR to receive money and send it to the user
-            // We handle it in another function
-            // const qr = await generateQR(recipientPhone);
-            const message = `Aqui tienes tu QR para recibir dinero.`;
-            // await sendQRToPhoneNumber(recipientPhone, message, qr);
-            await sendMessageToPhoneNumber(recipientPhone, message);
-            break;
+            await sendMessageToPhoneNumber(
+              recipientPhone,
+              'Recibiendo dinero...',
+            )
+            break
           case 'send_money':
             // do something
-            await sendMessageToPhoneNumber(recipientPhone, `Hemos enviado tu dinero. Tu saldo es ...`);
-            break;
+            await sendMessageToPhoneNumber(
+              recipientPhone,
+              `Hemos enviado tu dinero. Tu saldo es ...`,
+            )
+            break
           case 'check_balance':
             // do something
-            await sendMessageToPhoneNumber(recipientPhone, `Tu balance es ...`);
-            break;
-          case 'check_transactions':
-            // do something
-            await sendMessageToPhoneNumber(recipientPhone, `Tus transacciones son ...`);
-            break;
+            await sendMessageToPhoneNumber(recipientPhone, `Tu balance es ...`)
+            break
           default:
-            break;
+            break
         }
       }
 
@@ -90,11 +89,11 @@ const handler: VercelApiHandler = async (
       await Whatsapp.markMessageAsRead({
         message_id: messageId,
       })
-    }
 
-    res.status(200).send('ok')
-    return
-  }} catch (error) {
+      res.status(200).send('ok')
+      return
+    }
+  } catch (error) {
     console.error({ error })
     res.status(500)
     return
