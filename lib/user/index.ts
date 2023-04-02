d6ee667f7225576138bd9c533ac247ff7bcb21f5
sebipapsu <sebipaps@gmail.com>
2023-04-02 07:15:49 -0300

    fix.

--- lib/user/index.ts
+++ lib/user/index.ts
@@ -78,18 +78,18 @@ export async function createUser(
 export async function getUserFromPhoneNumber(
   recipientPhone: string,
 ): Promise<User | null> {
-  // const sanitizedPhoneNumber = recipientPhone
-  //   .replace('+', '')
-  //   .replace(' ', '')
-  //   .replace('-', '')
-  //   .replace('(', '')
-  //   .replace(')', '')
+  const sanitizedPhoneNumber = recipientPhone
+    .replace('+', '')
+    .replace(' ', '')
+    .replace('-', '')
+    .replace('(', '')
+    .replace(')', '')
 
   const { data: users, error } = await supabase
     .from('users')
     .select('*')
     // .eq('phone_number', recipientPhone)
-    .like('phone_number', `%${recipientPhone}%`)
+    .like('phone_number', `%${sanitizedPhoneNumber}%`)
 
   if (error) {
     throw new Error(`Error getting user from phone number ${error}`)
