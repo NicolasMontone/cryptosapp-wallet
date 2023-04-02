208a057995810968d0c14ae5d113db6119a4c087
NicolasMontone <montonenicolas01@gmail.com>
2023-04-02 07:33:34 -0300

    fix: leave ".".

---
+++ lib/utils/number.ts
@@ -0,0 +1,13 @@
+/**
+ * return a number from a string value, removing all non numeric characters
+ * @param value
+ * @returns
+ */
+export function transformStringToNumber(value: string): number {
+  const number = Number(value.replace(/[^0-9.]/g, ''))
+  if (Number.isNaN(number)) {
+    throw new Error(`Invalid number ${value}`)
+  }
+
+  return number
+}
--- lib/utils/string.ts
+++ lib/utils/string.ts
@@ -12,17 +12,3 @@ export function normalizeString(value: string): string {
     .toLowerCase()
     .trim()
 }
-
-/**
- * return a number from a string value, removing all non numeric characters
- * @param value
- * @returns
- */
-export function transformStringToNumber(value: string): number {
-  const number = Number(value.replace(/\D/g, ''))
-  if (Number.isNaN(number)) {
-    throw new Error(`Invalid number ${value}`)
-  }
-
-  return number
-}
