b9ce792bafd67ebe2d47602611a2d9835660b66d
sebipapsu <sebipaps@gmail.com>
2023-04-02 07:11:51 -0300

    fix: use like.

--- lib/user/index.ts
+++ lib/user/index.ts
@@ -88,8 +88,8 @@ export async function getUserFromPhoneNumber(
   const { data: users, error } = await supabase
     .from('users')
     .select('*')
-    .eq('phone_number', recipientPhone)
-  // .like('phone_number', `%${sanitizedPhoneNumber}%`)
+    // .eq('phone_number', recipientPhone)
+    .like('phone_number', `%${recipientPhone}%`)
 
   if (error) {
     throw new Error(`Error getting user from phone number ${error}`)
